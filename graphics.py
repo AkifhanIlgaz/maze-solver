abe417f43a9037fbb59d4ccd7987241db02de7a2
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-02-27 18:09:16 +0300

    Add window class

---
+++ graphics.py
@@ -0,0 +1,48 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.is_running = False
+
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+
+        while self.is_running:
+            self.redraw()
+
+        print("window closed...")
+
+    def close(self):
+        self.is_running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x,
+                           self.p2.y, fill=fill_color, width=2)
+
+        canvas.pack(fill=BOTH, expand=1)
